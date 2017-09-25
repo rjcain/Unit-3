4991489a8a145a2e795986df65e725e47c9de18a
rjcain <31353104+rjcain@users.noreply.github.com>
2017-09-25 11:35:20 -0700

    FInal Commit

--- lab_6/pair_programming/BeeperBot.java
+++ lab_6/pair_programming/BeeperBot.java
@@ -10,13 +10,37 @@ public class BeeperBot extends Robot
     //instance variable to store beeper count
     int[] beeperList = new int[13];
     int tempCount = 0;
+    int avenue = 1;
     
     public BeeperBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
     
     public void findAndCountBeepers() {
-        
+        while(frontIsClear()) {
+            if(nextToABeeper()) {
+                 steeple();
+            }
+            move();
+            avenue++;
+        }
+    }
+    
+    public void steeple() {
+        turnLeft();
+        while(nextToABeeper()) {
+            pickBeeper();
+            tempCount++;
+            move();
+        }
+        beeperList[avenue] = tempCount;
+        turnLeft();
+        turnLeft();
+        while(frontIsClear()) {
+            move();
+        }
+        turnLeft();
+        tempCount = 0;
     }
     
     public int[] getBeeperList() {
